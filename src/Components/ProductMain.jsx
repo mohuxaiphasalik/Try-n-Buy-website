1c9718b24d45c5f9d2810b54803e00624ec1f904
Huzaifa Salik <huzaifasalik786@gmail.com>
2022-12-27 18:43:46 +0500

    Before Making Separate Component of Sidebar

---
+++ src/Components/ProductMain.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+export default function ProductMain() {
+    return (
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-sm-4 col-11 offset-sm-0 offset-1">
+                    <div class="card">
+                        <img class="card-img-top" src="https://images.pexels.com/photos/963486/pexels-photo-963486.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
+                        <div class="card-body">
+                            <p class="card-text">Wooden chair with legs</p>
+                            <p>$90</p>
+                            <span class="fa fa-circle" id="red"></span>
+                            <span class="fa fa-circle" id="teal"></span>
+                            <span class="fa fa-circle" id="blue"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 offset-lg-0 col-sm-4 offset-sm-2 col-11 offset-1">
+                    <div class="card">
+                        <img class="card-img-top" src="https://images.pexels.com/photos/1125137/pexels-photo-1125137.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
+                        <div class="card-body">
+                            <p class="card-text">Ugly chair and table set</p>
+                            <p>$100</p>
+                            <span class="fa fa-circle" id="red"></span>
+                            <span class="fa fa-circle" id="teal"></span>
+                            <span class="fa fa-circle" id="blue"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-11 offset-sm-0 offset-1">
+                    <div class="card">
+                        <img class="card-img-top" src="https://images.pexels.com/photos/3757055/pexels-photo-3757055.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
+                        <div class="card-body">
+                            <p class="card-text">Leather Lounger</p>
+                            <p>$950</p>
+                            <span class="fa fa-circle" id="red"></span>
+                            <span class="fa fa-circle" id="teal"></span>
+                            <span class="fa fa-circle" id="blue"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-4 offset-lg-0 offset-sm-2 col-11 offset-1">
+                    <div class="card">
+                        <img class="card-img-top" src="https://images.unsplash.com/photo-1537182534312-f945134cce34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Card image cap" />
+                        <div class="card-body">
+                            <p class="card-text">Tree Trunk table set</p>
+                            <p>$390</p>
+                            <span class="fa fa-circle" id="red"></span>
+                            <span class="fa fa-circle" id="teal"></span>
+                            <span class="fa fa-circle" id="blue"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-lg-3 col-sm-4 col-11 offset-sm-0 offset-1">
+                    <div class="card">
+                        <img class="card-img-top" src="https://images.pexels.com/photos/3230274/pexels-photo-3230274.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
+                        <div class="card-body">
+                            <p class="card-text">Red Leather Bar Stool</p>
+                            <p>$30</p>
+                            <span class="fa fa-circle" id="red"></span>
+                            <span class="fa fa-circle" id="teal"></span>
+                            <span class="fa fa-circle" id="blue"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-4 offset-lg-0 offset-sm-2 col-11 offset-1">
+                    <div class="card">
+                        <img class="card-img-top" src="https://images.pexels.com/photos/3773571/pexels-photo-3773571.png?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
+                        <div class="card-body">
+                            <p class="card-text">Modern Dining Table</p>
+                            <p>$740</p>
+                            <span class="fa fa-circle" id="red"></span>
+                            <span class="fa fa-circle" id="teal"></span>
+                            <span class="fa fa-circle" id="blue"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-4 col-11 offset-sm-0 offset-1">
+                    <div class="card">
+                        <img class="card-img-top" src="https://images.pexels.com/photos/534172/pexels-photo-534172.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
+                        <div class="card-body">
+                            <p class="card-text">Boring Dining Table</p>
+                            <p>$760</p>
+                            <span class="fa fa-circle" id="red"></span>
+                            <span class="fa fa-circle" id="teal"></span>
+                            <span class="fa fa-circle" id="blue"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-4 offset-lg-0 offset-sm-2 col-11 offset-1">
+                    <div class="card">
+                        <img class="card-img-top" src="https://images.pexels.com/photos/37347/office-sitting-room-executive-sitting.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
+                        <div class="card-body">
+                            <p class="card-text">An Ugly Office</p>
+                            <p>$90</p>
+                            <span class="fa fa-circle" id="red"></span>
+                            <span class="fa fa-circle" id="teal"></span>
+                            <span class="fa fa-circle" id="blue"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>)
+}
