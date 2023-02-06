f4fcffe5ca21c6958d2682f45c17d1e663acaa51
Huzaifa Salik <huzaifasalik786@gmail.com>
2023-02-06 13:02:06 +0500

    Product Page

---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+export default function ProductCard() {
+    return (
+        <h1>This is Product Card</h1>
+    )
+}
---
+++ src/Components/ProductRow.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProductCard from './ProductCard';
+export default function ProductRow() {
+    return (<>
+        <ProductCard />
+
+    </>)
+}
