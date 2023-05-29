38617567888b6dac05e510707efb336e6b98a314
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2023-05-29 12:07:25 +0500

    .

--- src/Components/ProductPageContent.jsx
+++ src/Components/ProductPageContent.jsx
@@ -4,8 +4,22 @@ import ProductCard from './ProductCard'
 import axios from 'axios';
 
 import '../styles/productPageContent.css'
-import { useEffect } from 'react';
 export default function ProductMain() {
+    console.log('request is being made')
+    const [productsData, setProductsData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:4000/products');
+                const data = response.data;
+                setProductsData(data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
 
@@ -24,3 +38,11 @@ export default function ProductMain() {
             ))}
         </div>)
 }
+
+// description: "Add some flair to your wardrobe with our vibrant red t-shirts!"
+// discount: "30% Off"
+// img: "../data/images/boyT1.png"
+// newPrice: 3143
+// originalPrice:5000
+// sale:true
+// title:"Red T-Shirt"
